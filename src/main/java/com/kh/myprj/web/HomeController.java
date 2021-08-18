09817702d79cce95837bfbe5308fb29fcd8f8b56
minji <kmj94658@naver.com>
2021-08-18 14:48:03 +0900

    초기화면구현 추가

--- src/main/java/com/kh/myprj/web/HomeController.java
+++ src/main/java/com/kh/myprj/web/HomeController.java
@@ -1,5 +1,12 @@
 package com.kh.myprj.web;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+	@GetMapping
+	public String home() {
+		return "home";
+	}
 }
